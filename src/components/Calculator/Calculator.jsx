fd79f7bee567ecb7f62a822085596626e67b822c
Aaron Burk <aarondburk@gmail.com>
2020-01-16 13:42:03 -0600

    fully functional calculator

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -15,14 +15,56 @@ class Calculator extends Component {
 
   callOperator = () => {
     console.log('call operation');
+    let { storedValue, displayValue, selectedOperator } = this.state;
+    displayValue = Number(displayValue);
+    storedValue = Number(storedValue);
+    switch (selectedOperator) {
+      case '+':
+        displayValue = storedValue + displayValue;
+        break;
+      case '-':
+        displayValue = storedValue - displayValue;
+        break;
+      case 'x':
+        displayValue = storedValue * displayValue;
+        break;
+      case '/':
+        displayValue = storedValue / displayValue;
+        break;
+      default:
+        displayValue = '0';
+    }
+    displayValue = displayValue.toString();
+    if (displayValue === "NaN" || displayValue === "Infinity") displayValue = "0";
+    storedValue = displayValue;
+    selectedOperator = '';
+    this.setState({ displayValue, storedValue, selectedOperator });
   }
 
-  setOperator = () => {
+  setOperator = value => {
+    let { selectedOperator, storedValue, displayValue } = this.state;
     console.log('set operation');
+    if (selectedOperator === '') {
+      storedValue = displayValue
+      selectedOperator = value;
+      displayValue = '0';
+    } else {
+      selectedOperator = value;
+    }
+    this.setState({ selectedOperator, storedValue, displayValue });
   }
 
-  updateDisplay = () => {
+  updateDisplay = value => {
+    let { displayValue } = this.state;
     console.log('update display');
+    if (value === '.' && displayValue.includes('.')) value = '';
+    if (value === 'ce') {
+      displayValue = displayValue.substring(0,displayValue.length - 1);
+      if (displayValue === '') displayValue = '0';
+    } else {
+      displayValue === '0' ? displayValue = value : displayValue += value;
+    }
+    this.setState({ displayValue });
   }
 
   render = () => {
